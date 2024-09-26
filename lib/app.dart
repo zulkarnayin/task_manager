2ad0c2f76fff4db3569addad829d0d8022a82720
zulkarnayin <zulkarnayin2132@gmail.com>
2024-09-27 00:53:23 +0600

    first try

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/splash_screen.dart';
-import 'package:task_manager/ui/utils/app_colors.dart';
+//import 'package:task_manager/ui/utils/app_colors.dart';
 
 class TaskManagerApp extends StatefulWidget {
   const TaskManagerApp({super.key});
@@ -13,48 +13,48 @@ class _TaskManagerAppState extends State<TaskManagerApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorSchemeSeed: AppColors.themeColor,
-        textTheme: const TextTheme(),
-        inputDecorationTheme: _inputDecorationTheme(),
-        elevatedButtonTheme: _elevatedButtonThemeData(),
-      ),
+      // theme: ThemeData(
+      //   colorSchemeSeed: AppColors.themeColor,
+      //   textTheme: const TextTheme(),
+      //   inputDecorationTheme: _inputDecorationTheme(),
+      //   elevatedButtonTheme: _elevatedButtonThemeData(),
+      // ),
       home: const SplashScreen(),
     );
   }
 
-  ElevatedButtonThemeData _elevatedButtonThemeData() {
-    return ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.themeColor,
-        foregroundColor: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        fixedSize: const Size.fromWidth(double.maxFinite),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
-    );
-  }
-
-  InputDecorationTheme _inputDecorationTheme() {
-    return InputDecorationTheme(
-      fillColor: Colors.white,
-      filled: true,
-      hintStyle: const TextStyle(
-          fontWeight: FontWeight.w300
-      ),
-      border: _inputBorder(),
-      enabledBorder: _inputBorder(),
-      errorBorder: _inputBorder(),
-      focusedBorder: _inputBorder(),
-    );
-  }
-
-  OutlineInputBorder _inputBorder() {
-    return OutlineInputBorder(
-      borderSide: BorderSide.none,
-      borderRadius: BorderRadius.circular(8),
-    );
-  }
+  // ElevatedButtonThemeData _elevatedButtonThemeData() {
+  //   return ElevatedButtonThemeData(
+  //     style: ElevatedButton.styleFrom(
+  //       backgroundColor: AppColors.themeColor,
+  //       foregroundColor: Colors.white,
+  //       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+  //       fixedSize: const Size.fromWidth(double.maxFinite),
+  //       shape: RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.circular(8),
+  //       ),
+  //     ),
+  //   );
+  // }
+  //
+  // InputDecorationTheme _inputDecorationTheme() {
+  //   return InputDecorationTheme(
+  //     fillColor: Colors.white,
+  //     filled: true,
+  //     hintStyle: const TextStyle(
+  //         fontWeight: FontWeight.w300
+  //     ),
+  //     border: _inputBorder(),
+  //     enabledBorder: _inputBorder(),
+  //     errorBorder: _inputBorder(),
+  //     focusedBorder: _inputBorder(),
+  //   );
+  // }
+  //
+  // OutlineInputBorder _inputBorder() {
+  //   return OutlineInputBorder(
+  //     borderSide: BorderSide.none,
+  //     borderRadius: BorderRadius.circular(8),
+  //   );
+  // }
 }
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:task_manager/ui/screens/sign_in_screen.dart';
-// import 'package:task_manager/ui/utils/assets_path.dart';
-// import 'package:task_manager/ui/widgets/screen_background.dart';
+ import 'package:task_manager/ui/utils/assets_path.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,18 +16,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _moveToNextScreen();
+   // _moveToNextScreen();
   }
 
-  Future<void> _moveToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 2));
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const SignInScreen(),
-      ),
-    );
-  }
+  // Future<void> _moveToNextScreen() async {
+  //   await Future.delayed(const Duration(seconds: 2));
+  //   Navigator.pushReplacement(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) => const SignInScreen(),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
